60311c4113f15650b72fc72cd27af47ddd0443b4
Ukaegbu Gray Nneji <graynneji@outlook.com>
2025-10-20 01:43:28 -0700

    finished

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  logLevel: "error",
   server: {
     port: 3000,
   },
